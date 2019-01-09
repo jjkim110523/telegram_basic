3666d36b524a30e69d00c1c574c03aa903ff0c57
jjkim110523 <snorlax0523>
2019-01-09 17:10:21 +0900

    basic finished

---
+++ ChatModel.py
@@ -0,0 +1,32 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+class TelegramBot:
+    def __init__(self, name, token):
+        self.core=telegram.Bot(token)
+        self.updater=Updater(token)
+        self.id="684198776"
+        self.name=name
+
+    def sendMessage(self, text):
+        self.core.sendMessage(chat_id=self.id, text=text)
+
+    def stop(self):
+        self.updater.start_polling()
+        self.updater.dispatcher.stop()
+        self.updater.job_queue.stop()
+        self.updater.stop()
+
+class BotTest(TelegramBot):
+    def __init__(self):
+        self.token='604582951:AAGCfEB-GsjenkFBE6eR04sK_Pzmd2kWjCc'
+        TelegramBot.__init__(self, "테스트",self.token)
+        self.updater.stop()
+
+    def add_handler(self, cmd, func):
+        self.updater.dispatcher.add_handler(CommandHandler(cmd,func))
+
+    def start(self):
+        self.sendMessage("테스트 봇 시험 운행 시작")
+        self.updater.start_polling()
+        self.updater.idle()
---
+++ Chatbot.py
@@ -0,0 +1,29 @@
+import telegram
+import sys
+import ChatModel
+
+#API 기본 설정
+test_token = '604582951:AAGCfEB-GsjenkFBE6eR04sK_Pzmd2kWjCc'
+test = telegram.Bot(token = test_token)
+updates = test.getUpdates()
+for u in updates:
+    print(u.message)
+
+def proc_alert(bot, update):
+    test.sendMessage("그러고도 잠이 오냐ㅋㅋㅋ")
+    sound=firecracker()
+    test.sendMessage(sound)
+    test.sendMessage("ㅋㅋㅋㅋㅋㅋ")
+
+def proc_stop(bot, update):
+    test.sendMessage("빠잉~")
+    test.stop()
+
+def firecracker():
+    return "에베베베베베베"
+
+
+test=ChatModel.BotTest()
+test.add_handler("alert", proc_alert)
+test.add_handler("stop",proc_stop)
+test.start()
